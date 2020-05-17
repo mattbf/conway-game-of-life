29e53972aa11d85fa8e58e544670a1bcedd3afc2
Matthew Behan-Fossey <matthewbf8@gmail.com>
2020-05-17 16:55:06 -0600

    Added react hook responsive styling

--- game/src/App.js
+++ game/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react'
+import useWindowSize from './Utils/useWindowSize';
+
+const mainStyle = {
+  backgroundColor: 'blue',
+  width: '100%',
+  height: '100vh',
+};
+
+const mainStyleMobile = {
+  backgroundColor: 'green',
+  width: '100%',
+  height: '100vh',
+};
+
+const gameBoardStyle = {
+
+}
 
 function App() {
+  const windowSize = useWindowSize()
+
   return (
-    <div>
+    <div style={windowSize.width < 650 ? mainStyleMobile : mainStyle}>
       Conway Game
+      <Button> Hello </Button>
     </div>
   );
 }
